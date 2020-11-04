62db986f530e9d3bd8d9bfa600d9363bb2909afa
akira.saito <kaikai4424@gmail.com>
2020-11-04 22:37:33 +0900

    Add DestroyServlet.java

--- src/controlles/DestroyServlet.java
+++ src/controlles/DestroyServlet.java
@@ -1,19 +1,24 @@
 package controlles;
 
 import java.io.IOException;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.DTO;
+import utils.DAO;
+
 /**
  * Servlet implementation class DestroyServlet
  */
 @WebServlet("/destroy")
 public class DestroyServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,20 +27,28 @@ public class DestroyServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())){
+            EntityManager em = DAO.createEntityManager();
+
+            DTO m = em.find(DTO.class,(Integer)(request.getSession().getAttribute("tasklist_id")));
+
+            em.getTransaction().begin();
+            em.remove(m);
+            em.getTransaction().commit();
+            em.close();
+
+            request.getSession().removeAttribute("tasklist_id");
+
+            response.sendRedirect(request.getContextPath() + "/index");
+
+
+        }
+    }
 
 }
